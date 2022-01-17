92c74960a3ca52cea4d5757d1ea81a8e3be43876
Craig Wilson <crgwilson@users.noreply.github.com>
2022-01-17 14:43:36 -0500

    Add a minimal module to download dotfiles repos using the yadm cli (#1)

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -13,3 +13,14 @@ def test_default_files(host):
 
     b = host.file("/usr/local/bin/yadm")
     assert b.is_symlink
+
+
+def test_default_cloned_dotfiles(host):
+    d = host.file("/root/.yadm")
+    assert d.is_directory
+
+    f = host.file("/root/.yadm/bootstrap")
+    assert f.is_file
+
+    d = host.file("/root/zsh")
+    assert d.is_directory
